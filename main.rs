e14065a4dfdda3a9e78cda822711fc17b5eb4010
ROLL <endroll2288@gmail.com>
2026-02-25 11:21:12 +0900

    Update main.rs

--- main.rs
+++ main.rs
@@ -7,3 +7,34 @@ use serde_json::json;
 use actix_files::NamedFile;
 
 use reqwest;
+
+#[derive(Deserialize,Debug)]
+struct ThreadRequest {
+   
+    id: String,
+    token: String,
+    text: String,
+}
+#[derive(Deserialize, Debug)]
+struct JoinRequest { 
+    id: String,
+    token: String,
+}
+
+#[get("/")] 
+async fn serve_html() -> actix_web::Result<NamedFile> {
+  
+    Ok(NamedFile::open("v2/home.html")?)
+}
+
+#[get("/threads")]
+async fn thread_page() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("v2/thread.html")?)
+}
+
+#[get("/review")]
+async fn review_page() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("v2/review.html")?)
+}
+
+
